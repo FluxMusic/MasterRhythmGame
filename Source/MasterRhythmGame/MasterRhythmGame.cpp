a9c4719ba6d804cf5c604a947cb66ef097527e19
FluxMusic <alex.huber743@gmail.com>
2025-03-14 17:01:03 +0100

    Added Unreal Project

---
+++ Source/MasterRhythmGame/MasterRhythmGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MasterRhythmGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MasterRhythmGame, "MasterRhythmGame" );
