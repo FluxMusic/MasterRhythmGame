057c6286eaf3ef418fef1b0d00aee562168d82bc
Kay Friese <Sweat.Fire@gmx.de>
2025-11-22 20:33:56 +0100

    Add Stuff for Graphic Settings

--- Source/MasterRhythmGame/Private/UI/Settings/GraphicSettingWidget.cpp
+++ Source/MasterRhythmGame/Private/UI/Settings/GraphicSettingWidget.cpp
@@ -2,4 +2,172 @@
 
 
 #include "UI/Settings/GraphicSettingWidget.h"
+#include "Components/Button.h"
+
+void UGraphicSettingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	MainMenu->OnClicked.AddDynamic(this, &UGraphicSettingWidget::ReturnMainMenuClicked);
+	MainMenu->OnHovered.AddDynamic(this, &UGraphicSettingWidget::ReturnMainMenuHovered);
+	MainMenu->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::ReturnMainMenuUnhovered);
+
+	WindowModeDown->OnClicked.AddDynamic(this, &UGraphicSettingWidget::WindowModeDownClicked);
+	WindowModeDown->OnHovered.AddDynamic(this, &UGraphicSettingWidget::WindowModeDownHovered);
+	WindowModeDown->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::WindowModeDownUnhovered);
+
+	WindowModeUp->OnClicked.AddDynamic(this, &UGraphicSettingWidget::WindowModeUpClicked);
+	WindowModeUp->OnHovered.AddDynamic(this, &UGraphicSettingWidget::WindowModeUpHovered);
+	WindowModeUp->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::WindowModeUpUnhovered);
+
+	ResolutionDown->OnClicked.AddDynamic(this, &UGraphicSettingWidget::ResolutionDownClicked);
+	ResolutionDown->OnHovered.AddDynamic(this, &UGraphicSettingWidget::ResolutionDownHovered);
+	ResolutionDown->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::ResolutionDownUnhovered);
+
+	ResolutionUp->OnClicked.AddDynamic(this, &UGraphicSettingWidget::ResolutionUpClicked);
+	ResolutionUp->OnHovered.AddDynamic(this, &UGraphicSettingWidget::ResolutionUpHovered);
+	ResolutionUp->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::ResolutionUpUnhovered);
+
+	GraphicDown->OnClicked.AddDynamic(this, &UGraphicSettingWidget::GraphicDownClicked);
+	GraphicDown->OnHovered.AddDynamic(this, &UGraphicSettingWidget::GraphicDownHovered);
+	GraphicDown->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::GraphicDownUnhovered);
+
+	GraphicUp->OnClicked.AddDynamic(this,&UGraphicSettingWidget::GraphicUpClicked);
+	GraphicUp->OnHovered.AddDynamic(this,&UGraphicSettingWidget::GraphicUpHovered);
+	GraphicUp->OnUnhovered.AddDynamic(this,&UGraphicSettingWidget::GraphicUpUnhovered);	
+
+	VSyncDown->OnClicked.AddDynamic(this, &UGraphicSettingWidget::VSyncDownClicked);
+	VSyncDown->OnHovered.AddDynamic(this, &UGraphicSettingWidget::VSyncDownHovered);
+	VSyncDown->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::VSyncDownUnhovered);
+
+	VSyncUp->OnClicked.AddDynamic(this, &UGraphicSettingWidget::VSyncUpClicked);
+	VSyncUp->OnHovered.AddDynamic(this, &UGraphicSettingWidget::VSyncUpHovered);
+	VSyncUp->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::VSyncUpUnhovered);
+
+	Apply->OnClicked.AddDynamic(this, &UGraphicSettingWidget::ApplyClicked);
+	Apply->OnHovered.AddDynamic(this, &UGraphicSettingWidget::ApplyHovered);
+	Apply->OnUnhovered.AddDynamic(this, &UGraphicSettingWidget::ApplyUnhovered);
+
+}
+
+void UGraphicSettingWidget::ReturnMainMenuClicked()
+{
+}
+
+void UGraphicSettingWidget::ReturnMainMenuHovered()
+{
+}
+
+void UGraphicSettingWidget::ReturnMainMenuUnhovered()
+{
+}
+
+void UGraphicSettingWidget::WindowModeDownClicked()
+{
+}
+
+void UGraphicSettingWidget::WindowModeDownHovered()
+{
+}
+
+void UGraphicSettingWidget::WindowModeDownUnhovered()
+{
+}
+
+void UGraphicSettingWidget::WindowModeUpClicked()
+{
+}
+
+void UGraphicSettingWidget::WindowModeUpHovered()
+{
+}
+
+void UGraphicSettingWidget::WindowModeUpUnhovered()
+{
+}
+
+void UGraphicSettingWidget::ResolutionDownClicked()
+{
+}
+
+void UGraphicSettingWidget::ResolutionDownHovered()
+{
+}
+
+void UGraphicSettingWidget::ResolutionDownUnhovered()
+{
+}
+
+void UGraphicSettingWidget::ResolutionUpClicked()
+{
+}
+
+void UGraphicSettingWidget::ResolutionUpHovered()
+{
+}
+
+void UGraphicSettingWidget::ResolutionUpUnhovered()
+{
+}
+
+void UGraphicSettingWidget::GraphicDownClicked()
+{
+}
+
+void UGraphicSettingWidget::GraphicDownHovered()
+{
+}
+
+void UGraphicSettingWidget::GraphicDownUnhovered()
+{
+}
+
+void UGraphicSettingWidget::GraphicUpClicked()
+{
+}
+
+void UGraphicSettingWidget::GraphicUpHovered()
+{
+}
+
+void UGraphicSettingWidget::GraphicUpUnhovered()
+{
+}
+
+void UGraphicSettingWidget::VSyncDownClicked()
+{
+}
+
+void UGraphicSettingWidget::VSyncDownHovered()
+{
+}
+
+void UGraphicSettingWidget::VSyncDownUnhovered()
+{
+}
+
+void UGraphicSettingWidget::VSyncUpClicked()
+{
+}
+
+void UGraphicSettingWidget::VSyncUpHovered()
+{
+}
+
+void UGraphicSettingWidget::VSyncUpUnhovered()
+{
+}
+
+void UGraphicSettingWidget::ApplyClicked()
+{
+}
+
+void UGraphicSettingWidget::ApplyHovered()
+{
+}
+
+void UGraphicSettingWidget::ApplyUnhovered()
+{
+}
+
 
--- Source/MasterRhythmGame/Public/UI/Settings/GraphicSettingWidget.h
+++ Source/MasterRhythmGame/Public/UI/Settings/GraphicSettingWidget.h
@@ -14,6 +14,97 @@ class MASTERRHYTHMGAME_API UGraphicSettingWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+
+	UFUNCTION()
+	void ReturnMainMenuClicked();
+
+	UFUNCTION()
+	void ReturnMainMenuHovered();
+
+	UFUNCTION()
+	void ReturnMainMenuUnhovered();
+
+	UFUNCTION()
+	void WindowModeDownClicked();
+
+	UFUNCTION()
+	void WindowModeDownHovered();
+
+	UFUNCTION()
+	void WindowModeDownUnhovered();
+
+	UFUNCTION()
+	void WindowModeUpClicked();
+
+	UFUNCTION()
+	void WindowModeUpHovered();
+
+	UFUNCTION()
+	void WindowModeUpUnhovered();
+
+	UFUNCTION()
+	void ResolutionDownClicked();
+
+	UFUNCTION()
+	void ResolutionDownHovered();
+
+	UFUNCTION()
+	void ResolutionDownUnhovered();
+
+	UFUNCTION()
+	void ResolutionUpClicked();
+
+	UFUNCTION()
+	void ResolutionUpHovered();
+
+	UFUNCTION()
+	void ResolutionUpUnhovered();
+
+	UFUNCTION()
+	void GraphicDownClicked();
+
+	UFUNCTION()
+	void GraphicDownHovered();
+
+	UFUNCTION()
+	void GraphicDownUnhovered();
+
+	UFUNCTION()
+	void GraphicUpClicked();
+
+	UFUNCTION()
+	void GraphicUpHovered();
+
+	UFUNCTION()
+	void GraphicUpUnhovered();
+
+	UFUNCTION()
+	void VSyncDownClicked();
+
+	UFUNCTION()
+	void VSyncDownHovered();
+
+	UFUNCTION()
+	void VSyncDownUnhovered();
+
+	UFUNCTION()
+	void VSyncUpClicked();
+
+	UFUNCTION()
+	void VSyncUpHovered();
+
+	UFUNCTION()
+	void VSyncUpUnhovered();
+
+	UFUNCTION()
+	void ApplyClicked();
+
+	UFUNCTION()
+	void ApplyHovered();
+
+	UFUNCTION()
+	void ApplyUnhovered();
+
 	UPROPERTY(EditAnywhere, meta = (BindWidget))
 	class UButton* MainMenu { nullptr };
 
@@ -46,4 +137,8 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TEnumAsByte<EWindowMode::Type> WindowMode = EWindowMode::Fullscreen;
+
+private:
+	//Native Constructor
+	virtual void NativeConstruct() override;
 };
