05860dd3c6e5db0a7fd884aa9ca00b6de2d9da58
Kay Friese <Sweat.Fire@gmx.de>
2025-12-02 22:24:10 +0100

    Rename BPM property to Bpm in GameCharacter
    Refactored the GameCharacter class to rename the BPM property to Bpm for consistency. Updated accessors and property redirects, and modified related Blueprint and map assets to reflect this change.

--- Source/MasterRhythmGame/Public/Character/GameCharacter.h
+++ Source/MasterRhythmGame/Public/Character/GameCharacter.h
@@ -15,26 +15,15 @@ UCLASS()
 class MASTERRHYTHMGAME_API AGameCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
 public:
 	// Sets default values for this character's properties
 	AGameCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	//// --- CameraActor accessors ---
-	//TSoftObjectPtr<ACameraActor> GetCameraActorSoft() const { return CameraActor; }
-	//ACameraActor* GetCameraActor() const { return CameraActor.Get(); }
-	//void SetCameraActorSoft(const TSoftObjectPtr<ACameraActor>& InCameraActor) { CameraActor = InCameraActor; }
 
 	// --- Health accessors ---
 	int32 GetHealth() const { return Health; }
@@ -44,28 +33,27 @@ public:
 	int32 GetDefended() const { return Defended; }
 	void SetDefended(int32 InDefended) { Defended = InDefended; }
 
-	//// --- QuartzClockHandle accessors ---
-	//TObjectPtr<UQuartzClockHandle> GetQuartzClockHandle() const { return QuartzClockHandle; }
-	//UQuartzClockHandle* GetQuartzClock() const { return QuartzClockHandle.Get(); }
-	//void SetQuartzClockHandle(TObjectPtr<UQuartzClockHandle> InHandle) { QuartzClockHandle = InHandle; }
+	// --- Bpm accessors ---
+	float GetBpm() const { return Bpm; }
+	void SetBpm(float InBpm) { Bpm = InBpm; }
 
-	// --- BPM accessors ---
-	float GetBPM() const { return BPM; }
-	void SetBPM(float InBPM) { BPM = InBPM; }
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<ACameraActor> CameraActor { nullptr };
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	//TObjectPtr<UQuartzClockHandle> QuartzClockHandle { nullptr };
 
 private:
-	//UPROPERTY(EditAnywhere)
-	//TSoftObjectPtr<ACameraActor> CameraActor { nullptr };
-
 	UPROPERTY(EditAnywhere)
 	int32 Health { 50 };
 
 	UPROPERTY(EditAnywhere)
 	int32 Defended { 0 };
 
-	//UPROPERTY(EditAnywhere)
-	//TObjectPtr<UQuartzClockHandle> QuartzClockHandle { nullptr };
-
 	UPROPERTY(EditAnywhere)
-	float BPM { 0 };
+	float Bpm { 0 };
 };
