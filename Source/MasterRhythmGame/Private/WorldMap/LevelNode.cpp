1846fc97a50f1a1323ac5696ac1c5c455f0afaf3
iLoveMyHydro <Sweat.Fire@gmx.de>
2026-01-07 20:19:40 +0100

    Refactor WorldMapNode to LevelNode and update references
    Renamed WorldMapNode to LevelNode, including source and header files, and updated class names and includes accordingly. Removed WorldMapNode.h and added LevelNode.h with basic actor functionality. Updated WorldMapHUD.cpp to comment out WorldMapWidget instantiation. Added BP_LevelNode blueprint asset and updated WorldMap.umap to reflect changes.

--- Source/MasterRhythmGame/Private/WorldMap/LevelNode.cpp
+++ Source/MasterRhythmGame/Private/WorldMap/LevelNode.cpp
@@ -1,12 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "WorldMap/WorldMapNode.h"
+#include "WorldMap/LevelNode.h"
 
 // Sets default values
-AWorldMapNode::AWorldMapNode()
+ALevelNode::ALevelNode()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
+
+// Called when the game starts or when spawned
+void ALevelNode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevelNode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
